f26efc02904d57deef1404bf9a821dabaf15751e
rrightmy <rrightmy@ucsd.edu>
2015-01-22 20:22:14 -0800

    Minor Changes to signup
    Added more fields to activity_sign_up.xml and global variables relevant
    to those fields to SignUpActivity.java

--- CSE110App/app/src/main/java/com/swaglords/cse110app/Login/SignUpActivity.java
+++ CSE110App/app/src/main/java/com/swaglords/cse110app/Login/SignUpActivity.java
@@ -4,15 +4,36 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.swaglords.cse110app.R;
 
 public class SignUpActivity extends ActionBarActivity {
 
+    private EditText username;
+    private EditText email;
+    private EditText password;
+    private EditText passwordRepeat;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Set up the sign up page
+        username = (EditText) findViewById(R.id.signUpUsername);
+        email = (EditText) findViewById(R.id.signUpEmail);
+        password = (EditText) findViewById(R.id.signUpPassword);
+        passwordRepeat = (EditText) findViewById(R.id.signUpPasswordRepeat);
+        firstName = (EditText) findViewById(R.id.signUpFirstName);
+        lastName = (EditText) findViewById(R.id.signUpLastName);
+        address = (EditText) findViewById(R.id.signUpAddress);
+
+        //Set up Register button click handler
+
     }
 
 
